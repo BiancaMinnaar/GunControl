3659e752b3c1e7e740b98a53ad698eef0cdbcb5e
Bianca Minnaar <bminnaar@gmail.com>
2018-06-29 14:21:21 +0200

    Update Template to build Lollipop. - Remove all shared projects and references to Bonsai - Add Forms >2.4.0.91020< - Add "Acr.UserDialogs" V 5.2.0 - Remove Icon from MainActivity

--- GunControl/Droid/MainActivity.cs
+++ GunControl/Droid/MainActivity.cs
@@ -1,12 +1,11 @@
 using Android.App;
 using Android.Content;
-using Android.Content.PM;
 using Android.OS;
 using Acr.UserDialogs;
 
 namespace GunControl.Droid
 {
-    [Activity(Theme = "@android:style/Theme.Material.Light", Label = "MyApp", MainLauncher = true, Icon = "@drawable/icon")]  
+    [Activity(Theme = "@android:style/Theme.Material.Light", Label = "MyApp", MainLauncher = true)]  
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -15,7 +14,7 @@ namespace GunControl.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             UserDialogs.Init(this);
-            LoadApplication(new App());
+            //LoadApplication(new App());
         }
 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
