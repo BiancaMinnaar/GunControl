dc7aa8df160cb7ee64bf9d527726c43e6acab5e8
Bianca Minnaar <bminnaar@gmail.com>
2018-07-16 06:36:21 +0200

    Complete Bonsai Upgrade - Add PlatformBonsai - Move RestService to REST folder

---
+++ GunControl/Droid/Injection/Base/PlatformBonsai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using GunControl.Droid.Injection.Base;
+using GunControl.Trunk.Injection.Base;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PlatformBonsai))]
+namespace GunControl.Droid.Injection.Base
+{
+    public sealed class PlatformBonsai : PlatformServiceBonsai<IPlatformModelBonsai>, IPlatformBonsai<IPlatformModelBonsai>
+    {
+        public new Action<IPlatformModelBonsai> ServiceCallBack
+        {
+            get => PlatformSingleton.Instance.ServiceCallBack;
+            set => PlatformSingleton.Instance.ServiceCallBack = value;
+        }
+
+        public IEnumerable<BonsaiPlatformServiceRegistrationStruct> GetBonsaiServices
+            => PlatformSingleton.Instance.PlatformServiceList.Values;
+
+        public static void NotifyOfBackgroundChange(IPlatformModelBonsai model)
+        {
+            PlatformSingleton.Instance.Model.IsBackgroundAvailable = model.IsBackgroundAvailable;
+            PlatformSingleton.Instance.Model.IsInBackground = model.IsInBackground;
+            PlatformSingleton.Instance.ServiceCallBack?.Invoke(PlatformSingleton.Instance.Model);
+        }
+    }
+}
